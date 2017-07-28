1a82ded7bada42b0fe4e3ffd6f9c8ce4bc2142e7
Leosocy <513887568@qq.com>
2017-07-29 00:38:24 +0800

    Update IO.h && IO.cc

--- include/EDCC/IO.h
+++ include/EDCC/IO.h
@@ -12,21 +12,36 @@
 #include <vector>
 #include <map>
 #include <string>
+#include <json/json.h>
 using namespace std;
 using namespace cv;
+
+#define LOAD_CONFIG_FAILURE 1
+#define LOAD_CONFIG_SUCCESS 0
+
+#define LOAD_PALMPRINT_GROUP_FAILURE 1
+#define LOAD_PALMPRINT_GROUP_SUCCESS 0
+
+#define LOAD_PALMPRINT_FEATURE_DATA_FAILURE 1
+#define LOAD_PALMPRINT_FEATURE_DATA_SUCCESS 0
+
+#define SAVE_PALMPRINT_FEATURE_DATA_FAILURE 1
+#define SAVE_PALMPRINT_FEATURE_DATA_SUCCESS 0
+
 namespace EDCC
 {
 	//=========================================================
 	/*
 	 *Input & Output Module 
 	 */
-	class Palmprint;
+//	class Palmprint;
+//	class PalmprintCode;
 	class IO {
 		public:
-			static void loadConfig( ifstream &in, vector< std::map< string, string > > configVec );
-			static void loadPalmprintGroup( ifstream &in, vector< Palmprint > groupVec );
-			static void loadPalmprintFeatureData( ifstream &in  );
-			static void savePalmprintFeatureData( ofstream &out );
+			static int loadConfig( ifstream &in, std::map< string, int > &configMap );
+//			static void loadPalmprintGroup( ifstream &in, vector< Palmprint > &groupVec );
+//			static void loadPalmprintFeatureData( ifstream &in, vector< PalmprintCode > data );
+//			static void savePalmprintFeatureData( ofstream &out, vector< PalmprintCode > data );
 	};
 	//=========================================================
 }
