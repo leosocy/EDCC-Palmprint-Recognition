86d6d128405ebac0df00520974f1d1ae47207d4c
Leosocy <513887568@qq.com>
2017-08-01 22:44:19 +0800

    add Match.h

---
+++ include/EDCC/Match.h
@@ -0,0 +1,18 @@
+/*************************************************************************
+	> File Name: Match.h
+	> Author: Leosocy
+	> Mail: 513887568@qq.com 
+	> Created Time: 2017/08/01 22:42:15
+ ************************************************************************/
+#ifndef __MATCH_H__
+#define __MATCH_H__
+#include <opencv2/opencv.hpp>
+#include <EDCC/Core.h>
+using namespace std;
+using namespace cv;
+
+namespace EDCC {
+
+}
+
+#endif 
