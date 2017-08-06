7aa1613f42292e64f4bcdc2aee4ddac66f92f05c
Leosocy <513887568@qq.com>
2017-08-06 22:42:01 +0800

    Complete IO interface

--- include/EDCC/IO.h
+++ include/EDCC/IO.h
@@ -39,18 +39,21 @@ namespace EDCC
 	class IO {
 		public:
 			map< string, int > paramsMap;
-			set< string > paramsSet;
 			
 			IO();
 
 			int loadConfig( ifstream &in  );
 			int loadPalmprintGroup( ifstream &in, vector< Palmprint > &groupVec );
 			int loadPalmprintFeatureData( ifstream &in, vector< PalmprintCode > &data );
-			int savePalmprintFeatureData( ofstream &out, const vector< PalmprintCode > &data );
+			int savePalmprintFeatureData( ofstream &out, vector< PalmprintCode > &data );
 		private:
-			int loadOneIdentityAllPalmprintFeatureData( const string &identity, const Json::Value &value, vector< PalmprintCode > &data );
+			set< string > paramsSet;
+			int loadOneIdentityAllPalmprintFeatureData( const string &identity, const Json::Value &value, vector< PalmprintCode > &data );			
 			void genEDCCoding( const Json::Value &value, EDCCoding &coding );
-			int jsonArray2Mat( const Json::Value &value, Mat &dst );
+			void jsonArray2Mat( const Json::Value &value, Mat &dst );
+			bool insert2JsonValue( PalmprintCode &code, Json::Value &value );
+			void setEDCCoding( const EDCCoding &coding, Json::Value &value );
+			void Mat2jsonArray( const Mat &src, Json::Value &value );
 	};
 	//=========================================================
 }
