47cc3be7b14e14dfdd9812126f5b05c50e59bebc
leosocy <513887568@qq.com>
2017-10-06 00:14:06 +0800

    add FT parse_cmd 当入参为训练，但是-t之后没有输入文件，则Adapter中对应标记为FALSE

--- test/src/ft_parse_cmd.cpp
+++ test/src/ft_parse_cmd.cpp
@@ -1,6 +1,11 @@
 #include <ft_parse_cmd.h>
 
-TEST_F(ft_parse_cmd, Given_PalmprintTrainGroup_When_With_Correct_CMD_Then_Check_Adapter_BoolTrain_True)
+TEST_F(ft_parse_cmd, Given_Correct_CMD_When_Train_Then_Check_Adapter_BoolTrain_True)
+{
+
+}
+
+TEST_F(ft_parse_cmd, Given_Correct_CMD_When_Train_Without_InputFile_Then_Check_Adapter_BoolTrain_False)
 {
 
 }
