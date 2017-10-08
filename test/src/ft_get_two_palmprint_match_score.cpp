5d805663594e6db4a22d458b192646f6578d59a7
leosocy <513887568@qq.com>
2017-10-08 13:00:44 +0800

    modify ft name

--- test/src/ft_get_two_palmprint_match_score.cpp
+++ test/src/ft_get_two_palmprint_match_score.cpp
@@ -1,6 +1,6 @@
 #include <ft_get_two_palmprint_match_score.h>
 
-TEST_F(ft_get_two_palmprint_match_score, Given_Some_Param_NULL_When_Get_Two_Match_Score_Then_EDCC_NULL_POINTER_ERROR)
+TEST_F(ft_get_two_palmprint_match_score, Given_Some_Param_NULL_When_Get_Score_Then_EDCC_NULL_POINTER_ERROR)
 {
     double matchScore = 0.0;
     int ret = EDCC::GetTwoPalmprintMatchScore(NULL,
