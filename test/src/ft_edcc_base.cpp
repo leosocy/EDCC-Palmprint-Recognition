0e5d0f98488cf166dc94b8f84773372410aef4fc
leosocy <513887568@qq.com>
2017-10-10 22:37:12 +0800

    add CopyDir DeleteDir for FT

--- test/src/ft_edcc_base.cpp
+++ test/src/ft_edcc_base.cpp
@@ -168,7 +168,11 @@ void ft_edcc_base::CopyFile(const char *srcFile, const char *dstFile)
     #ifdef _WINDOWS
     snprintf(opr, sizeof(opr), "echo f | xcopy /y %s %s > ft_log.txt", srcFile, dstFile);
     #else
+<<<<<<< HEAD
     snprintf(opr, sizeof(opr), "mv -f %s %s > ft_log.txt", srcDir, dstDir);
+=======
+    snprintf(opr, sizeof(opr), "mv -f %s %s > ft_log.txt", srcFile, dstFile);
+>>>>>>> add CopyDir DeleteDir for FT
     #endif
 
     system(opr);
@@ -193,7 +197,7 @@ void ft_edcc_base::DeleteFile(const char *srcFile)
     #ifdef _WINDOWS
     snprintf(opr, sizeof(opr), "del /f /s /q %s > ft_log.txt", srcFile);
     #else
-    snprintf(opr, sizeof(opr), "rm -rf %s > ft_log.txt", srcDir);
+    snprintf(opr, sizeof(opr), "rm -rf %s > ft_log.txt", srcFile);
     #endif
 
     system(opr);
